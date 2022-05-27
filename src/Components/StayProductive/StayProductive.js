49d572b89edf73d0054693df4a1d75fb2371cb86
Fabian Villasenor <exionite@gmail.com>
2022-05-27 13:15:43 -0700

    spacing

--- src/Components/StayProductive/StayProductive.js
+++ src/Components/StayProductive/StayProductive.js
@@ -5,14 +5,16 @@ import productive from "../../images/stay-productive.png"
 export default function StayProductive() {
   return (
     <div className='stayProductive-container'>
-      <div className="image-container">
-        <img src={productive} alt="" />
-      </div>
-      <div className="text-container">
-        <h1>Stay Productive, wherever you are</h1>
-        <p>Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage needs.</p>
-        <p>Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.</p>
-        <p>See how Fylo works</p>
+      <div className="image-text-container">
+        <div className="image-container">
+          <img src={productive} alt="" />
+        </div>
+        <div className="text-container">
+          <h1>Stay Productive, wherever you are</h1>
+          <p>Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage needs.</p>
+          <p>Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.</p>
+          <p>See how Fylo works</p>
+        </div>
       </div>
     </div>
   )
