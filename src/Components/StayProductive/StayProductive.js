365f9496f1b2e1211e80d2d16260bcb50c8c36d6
Fabian Villasenor <exionite@gmail.com>
2022-06-01 08:40:40 -0700

    stay productive

--- src/Components/StayProductive/StayProductive.js
+++ src/Components/StayProductive/StayProductive.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./StayProductive.scss"
 import productive from "../../images/stay-productive.png"
+import arrow from "../../images/icon-arrow.svg"
 
 export default function StayProductive() {
   return (
@@ -13,7 +14,7 @@ export default function StayProductive() {
           <h1>Stay Productive, wherever you are</h1>
           <p>Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage needs.</p>
           <p>Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.</p>
-          <p>See how Fylo works</p>
+          <p id='see-how-works'>See how Fylo works <img src={arrow} alt="small arrow"/></p>
         </div>
       </div>
     </div>
