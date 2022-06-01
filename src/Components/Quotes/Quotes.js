fde5648509a330c26d0780c48ec4239e30dfd474
Fabian Villasenor <exionite@gmail.com>
2022-06-01 10:38:28 -0700

    testimonials

--- src/Components/Quotes/Quotes.js
+++ src/Components/Quotes/Quotes.js
@@ -1,16 +1,56 @@
 import React from 'react';
 import "./Quotes.scss"
+import Satish from "../../images/profile-1.jpg"
+import Bruce from "../../images/profile-2.jpg"
+import Iva from "../../images/profile-3.jpg"
+
+const text = "Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine";
+const authorTitle = "Founder & CEO, Huddle";
+
+const testimonials = [
+  {
+    id: 0,
+    image: Satish,
+    author: "Satish Patel",
+  },
+  {
+    id: 1,
+    image: Bruce,
+    author: "Bruce McKenzie",
+  },
+  {
+    id: 2,
+    image: Iva,
+    author: "Iva Boyd",
+  },
+]
 
 const Quotes = () => {
   return (
     <div className='quotes-container'>
       <div className="testimonials-container">
-        <div className="testimonials">1</div>
-        <div className="testimonials">2</div>
-        <div className="testimonials">3</div>
+        {testimonials.map((testimonial) => (
+          <div className="testimonials" id={testimonial.id}>
+            <div className="testimonials-padding">
+              <p>{text}</p>
+              <img src={testimonial.image} alt={testimonial.author} />
+              <p className='author'>{testimonial.author}</p>
+              <p className='author'>{authorTitle}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
 }
 
 export default Quotes;
+
+// {features.map((feature) => (
+//   <div className="feature">
+//     <img src={process.env.PUBLIC_URL + feature.image } alt="" />
+//     <h2>{feature.title}</h2>
+//     <p>{feature.text}</p>
+//   </div>
+// )
+// )}
